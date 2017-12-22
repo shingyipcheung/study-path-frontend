44659ae3f50f435797d85e64f70622612f0a287f
James <shingyipcheung@gmail.com>
2017-12-22 14:12:39 +0800

    use updated backend

--- src/api/backend_axios.js
+++ src/api/backend_axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://127.0.0.1:8000'
+// const baseUrl = 'http://127.0.0.1:8000'
+const baseUrl = 'http://143.89.76.37:8000'
 
 export default {
   fetchConcepts () {
